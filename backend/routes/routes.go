56ae0b858936b955e494bf757675fe4d01c1d043
Brena <brelaurindo@gmail.com>
2025-05-05 20:25:13 -0300

    Atualização das rotas para condizer com cada UserType

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -7,91 +7,104 @@ import (
 	"strings"
 
 	"github.com/BlogGFIG/BlogGFIG/controllers"
+	"github.com/BlogGFIG/BlogGFIG/middlewares"
+	//"github.com/go-playground/locales/mas"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 // HandleRequest configura todas as rotas do servidor
 func HandleRequest() {
+	
+	// Rota acessível para usuários não autenticados
 	r := mux.NewRouter()
 
-	// Rota para o registro do usuário
+	// Rota acessível apenas para o usuário master
+	masterRoutes := r.PathPrefix("/master").Subrouter()
+	masterRoutes.Use(middlewares.Authorize("master"))
+
+	// Rota acessível apenas para o administrador
+    adminRoutes := r.PathPrefix("/admin").Subrouter()
+    adminRoutes.Use(middlewares.Authorize("admin", "master"))
+
+	// Rota acessível para qualquer usuário (master, admin ou user)
+	anyUserRoutes := r.PathPrefix("/anyUser").Subrouter()
+	anyUserRoutes.Use(middlewares.Authorize("master", "admin", "user"))
+
+	// Rota para o registro do usuário (TODOS) (POST)
 	r.HandleFunc("/register", controllers.Register).Methods("POST")
 
-	// Rota para o login do usuário
+	// Rota para o login do usuário (TODOS) (POST)
 	r.HandleFunc("/login", controllers.Login).Methods("POST")
 
-	// Rota para a atualização do perfil
-	r.HandleFunc("/perfilPage", controllers.RefreshBio).Methods("POST")
-
-	// Rota para a atualização da senha
-	r.HandleFunc("/senhaPage", controllers.RefreshPassword).Methods("POST")
+	// Rota para a atualização da senha (USUÁRIO AUTENTICADO) (PUT)
+	anyUserRoutes.HandleFunc("/senhaPage", controllers.RefreshPassword).Methods("POST")
 
 	// Rota para obter os padrões de privacidade
-	r.HandleFunc("/padroesDePrivacidade", controllers.PrivacyCombo).Methods("GET")
+	//r.HandleFunc("/padroesDePrivacidade", controllers.PrivacyCombo).Methods("GET")
 
 	// Rota para adicionar novos padrões de privacidade
-	r.HandleFunc("/padroesDePrivacidade", controllers.ADDPrivacyCombo).Methods("POST")
+	//r.HandleFunc("/padroesDePrivacidade", controllers.ADDPrivacyCombo).Methods("POST")
 
 	// Rota para obter postagens sensíveis
-	r.HandleFunc("/sensibilidadeDeConteudoPage", controllers.SensitivityPost).Methods("GET")
+	//r.HandleFunc("/sensibilidadeDeConteudoPage", controllers.SensitivityPost).Methods("GET")
 
 	// Rota para obter visibilidade do perfil
-	r.HandleFunc("/visibilidadeDoPerfilPage", controllers.ProfileVisibility).Methods("GET")
+	//r.HandleFunc("/visibilidadeDoPerfilPage", controllers.ProfileVisibility).Methods("GET")
 
 	// Rota para adicionar visibilidade do perfil
-	r.HandleFunc("/visibilidadeDoPerfilPage", controllers.ADDProfileVisibility).Methods("POST")
+	//r.HandleFunc("/visibilidadeDoPerfilPage", controllers.ADDProfileVisibility).Methods("POST")
 
-	// Rota para redefinir senha
+	// Rota para redefinir senha (TODOS) (POST)
 	r.HandleFunc("/resetPassword", controllers.ResetPassword).Methods("POST")
 
-	// Rota para atualizar senha
+	// Rota para atualizar senha (TODOS) (POST)
 	r.HandleFunc("/updatePassword", controllers.UpdatePassword).Methods("POST")
 
-	// Rota para criar uma postagem (POST)
-	r.HandleFunc("/posts", controllers.CreatePost).Methods("POST")
+	// Rota para criar uma postagem (USUÁRIO AUTENTICADO) (POST)
+	anyUserRoutes.HandleFunc("/posts", controllers.CreatePost).Methods("POST")
 
-	// Rota para editar uma postagem (PUT)
-	r.HandleFunc("/edit-post", controllers.EditPost).Methods("PUT")
+	// Rota para editar uma postagem (USUÁRIO AUTENTICADO) (PUT)
+	anyUserRoutes.HandleFunc("/edit-post", controllers.EditPost).Methods("PUT")
 
-	// Rota para deletar uma postagem (DELETE)
-	r.HandleFunc("/delete-post", controllers.DeletePost).Methods("DELETE")
+	// Rota para deletar uma postagem (USUÁRIO AUTENTICADO) (DELETE)
+	anyUserRoutes.HandleFunc("/delete-post", controllers.DeletePost).Methods("DELETE")
 
-	// Rota para buscar todas as postagens (GET)
+	// Rota para buscar todas as postagens (TODOS) (GET)
 	r.HandleFunc("/posts", controllers.GetPosts).Methods("GET")
 
-	// Rota para fixar uma postagem (PUT)
-	r.HandleFunc("/pin-post", controllers.PinPost).Methods("PUT")
+	// Rota para fixar uma postagem (USUÁRIO AUTENTICADO) (PUT)
+	anyUserRoutes.HandleFunc("/pin-post", controllers.PinPost).Methods("PUT")
 
-	// Rota para desfixar uma postagem (PUT)
-	r.HandleFunc("/unpin-post", controllers.UnpinPost).Methods("PUT")
+	// Rota para desfixar uma postagem (USUÁRIO AUTENTICADO) (PUT)
+	anyUserRoutes.HandleFunc("/unpin-post", controllers.UnpinPost).Methods("PUT")
 
-	// Rota para criar um comentário em uma postagem (POST)
+	// Rota para criar um comentário em uma postagem (TODOS) (POST)
 	r.HandleFunc("/create-comment", controllers.CreateComment).Methods("POST")
 
-	// Rota para editar um comentário (PUT)
-	r.HandleFunc("/edit-comment", controllers.EditComment).Methods("PUT")
+	// Rota para editar um comentário (USUÁRIO AUTENTICADO) (PUT)
+	anyUserRoutes.HandleFunc("/edit-comment", controllers.EditComment).Methods("PUT")
 
-	// Rota para deletar um comentário (DELETE)
-	r.HandleFunc("/delete-comment", controllers.DeleteComment).Methods("DELETE")
+	// Rota para deletar um comentário (USUÁRIO AUTENTICADO) (DELETE)
+	anyUserRoutes.HandleFunc("/delete-comment", controllers.DeleteComment).Methods("DELETE")
 
-	// Rota para listar comentários de uma postagem específica (GET)
+	// Rota para listar comentários de uma postagem específica (TODOS) (GET)
 	r.HandleFunc("/list-comments/{postId}", controllers.ListComments).Methods("GET")
 
-	// Rota para listar os usuários (GET)
-	r.HandleFunc("/users", controllers.GetUsers).Methods("GET")
+	// Rota para listar os usuários (ADMIN E MASTER) (GET)
+	adminRoutes.HandleFunc("/users", controllers.GetUsers).Methods("GET")
 
-	// Rota para alterar status do usuário entre ativo e inativo (PUT)
-	r.HandleFunc("/toggleUserStatus", controllers.ToggleUserStatus).Methods("PUT")
+	// Rota para alterar status do usuário entre ativo e inativo (ADMIN E MASTER) (PUT)
+	adminRoutes.HandleFunc("/ativarOuInativar", controllers.AtivarOuInativar).Methods("PUT")
 
-	// Rota para aprovar/reprovar usuário (PUT)
-	r.HandleFunc("/approveOrRejectUser", controllers.ApproveOrRejectUser).Methods("PUT")
+	// Rota para aprovar/reprovar usuário (ADMIN E MASTER) (PUT)
+	adminRoutes.HandleFunc("/approveOrRejectUser", controllers.ApproveOrRejectUser).Methods("PUT")
 
-	// Rota para atualizar a role de um usuário (PUT)
-	r.HandleFunc("/updateUserRole", controllers.UpdateUserRole).Methods("PUT")
+	// Rota para atualizar a role de um usuário (MASTER) (PUT)
+	masterRoutes.HandleFunc("/updateUserRole", controllers.UpdateUserRole).Methods("PUT")
 
-	// Rota para obter o tipo de usuário pelo e-mail (GET)
-	r.HandleFunc("/get-user-type", controllers.GetUserTypeByEmail).Methods("GET")
+	// Rota para obter o tipo de usuário pelo e-mail (TODOS) (GET)
+	r.HandleFunc("/get-user-type", controllers.GetUserTypeByToken).Methods("GET")
 
 	// Configuração do CORS
 	corsHandler := handlers.CORS(
